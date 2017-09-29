cba4fef192d873390a4a08c8fad3b4bd02bc0a2a
Pranav Deshpande <pranavdeshpande12592@gmail.com>
2017-09-29 10:57:28 -0700

    first Commit

--- Testing/AppDelegate.swift
+++ Testing/AppDelegate.swift
@@ -6,17 +6,35 @@
 //  Copyright © 2017 Pranav Shashikant Deshpande. All rights reserved.
 //
 
+import Google
+import GoogleSignIn
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    
+    func applicationDidFinishLaunching(_ application: UIApplication) {
+        // Initialize sign-in
+        var configureError: NSError?
+        GGLContext.sharedInstance().configureWithError(&configureError)
+        assert(configureError == nil, "Error configuring Google services: \(configureError)")
+    }
+    func application(_ application: UIApplication,
+                     open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return GIDSignIn.sharedInstance().handle(url,
+                                                 sourceApplication: sourceApplication,
+                                                 annotation: annotation)
+    }
+    @available(iOS 9.0, *)
+    func application(_ app: UIApplication, open url: URL,
+                     options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
+        let sourceApplication = options[UIApplicationOpenURLOptionsKey.sourceApplication] as? String
+        let annotation = options[UIApplicationOpenURLOptionsKey.annotation]
+        return GIDSignIn.sharedInstance().handle(url,
+                                                 sourceApplication: sourceApplication,
+                                                 annotation: annotation)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- Testing/ViewController.swift
+++ Testing/ViewController.swift
@@ -6,14 +6,98 @@
 //  Copyright © 2017 Pranav Shashikant Deshpande. All rights reserved.
 //
 
+import GoogleAPIClientForREST
+import GoogleSignIn
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate {
+    
+    private let scopes = [kGTLRAuthScopeYouTubeReadonly]
+    
+    private let service = GTLRYouTubeService()
+    let signInButton = GIDSignInButton()
+    let output = UITextView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Configure Google Sign-in.
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().scopes = scopes
+        GIDSignIn.sharedInstance().signInSilently()
+        
+        // Add the sign-in button.
+        view.addSubview(signInButton)
+        
+        // Add a UITextView to display output.
+        output.frame = view.bounds
+        output.isEditable = false
+        output.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
+        output.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        output.isHidden = true
+        view.addSubview(output);
+        
     }
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
+              withError error: Error!) {
+        if let error = error {
+            showAlert(title: "Authentication Error", message: error.localizedDescription)
+            self.service.authorizer = nil
+        } else {
+            self.signInButton.isHidden = true
+            self.output.isHidden = false
+            self.service.authorizer = user.authentication.fetcherAuthorizer()
+            fetchChannelResource()
+        }
+    }
+    func fetchChannelResource() {
+        let query = GTLRYouTubeQuery_ChannelsList.query(withPart: "snippet,statistics")
+        query.identifier = "UC_x5XG1OV2P6uZZ5FSM9Ttw"
+        // To retrieve data for the current user's channel, comment out the previous
+        // line (query.identifier ...) and uncomment the next line (query.mine ...)
+        // query.mine = true
+        service.executeQuery(query,
+                             delegate: self,
+                             didFinish: #selector(displayResultWithTicket(ticket:finishedWithObject:error:)))
+    }
+    @objc func displayResultWithTicket(
+        ticket: GTLRServiceTicket,
+        finishedWithObject response : GTLRYouTube_ChannelListResponse,
+        error : NSError?) {
+        
+        if let error = error {
+            showAlert(title: "Error", message: error.localizedDescription)
+            return
+        }
+        
+        var outputText = ""
+        if let channels = response.items, !channels.isEmpty {
+            let channel = response.items![0]
+            let title = channel.snippet!.title
+            let description = channel.snippet?.descriptionProperty
+            let viewCount = channel.statistics?.viewCount
+            outputText += "title: \(title!)\n"
+            outputText += "description: \(description!)\n"
+            outputText += "view count: \(viewCount!)\n"
+        }
+        output.text = outputText
+    }
+    func showAlert(title : String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: UIAlertControllerStyle.alert
+        )
+        let ok = UIAlertAction(
+            title: "OK",
+            style: UIAlertActionStyle.default,
+            handler: nil
+        )
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
